7f545bfc6cb2fa488babee8b1ffd768ff733d7ca
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-01-09 11:38:33 -0300

    feat: modify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,30 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
   env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
+    browser: true ,
+    es6: true ,
+    node: true ,
+  } ,
+  extends: [
+    'eslint:recommended' ,
+    'plugin:@typescript-eslint/eslint-recommended' ,
+    'plugin:@typescript-eslint/recommended' ,
+  ] ,
+  globals: {
+    Atomics: 'readonly' ,
+    SharedArrayBuffer: 'readonly' ,
+  } ,
+  parser: '@typescript-eslint/parser' ,
+  parserOptions: {
+    ecmaVersion: 11 ,
+    sourceType: 'module' ,
+  } ,
+  plugins: ['@typescript-eslint'] ,
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    quotes: ['error' , 'single'] ,
+    'no-throw-literal': 1 ,
+    '@typescript-eslint/no-unused-vars': 0 ,
+    'linebreak-style': 0 ,
+    semi: ['error' , 'always'] ,
+    'no-console': ['off'] ,
+  } ,
 };
