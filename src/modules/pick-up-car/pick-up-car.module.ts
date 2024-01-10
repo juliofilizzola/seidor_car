173bd470064fcb73e2a95ba490c768114715db23
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-01-09 22:49:44 -0300

    feat: add providers and create .editorconfig

--- src/modules/pick-up-car/pick-up-car.module.ts
+++ src/modules/pick-up-car/pick-up-car.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PickUpCarService } from './pick-up-car.service';
 import { PickUpCarController } from './pick-up-car.controller';
+import { DriverService } from '../driver/driver.service';
+import { CarService } from '../car/car.service';
 
 @Module({
   controllers: [PickUpCarController],
-  providers: [PickUpCarService],
+  providers: [
+    PickUpCarService,
+    DriverService,
+    CarService
+  ],
 })
 export class PickUpCarModule {}
