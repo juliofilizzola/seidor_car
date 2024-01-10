dfd3a203412c93dc5908906d6937daaa875b8459
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-01-09 22:46:09 -0300

    feat: modify main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app
+    .listen(process.env.PORT)
+    .then(() => console.log(`app running on port: ${process.env.PORT}`));
 }
 bootstrap();
